acc694856631680129bfa7f23bcbe6e84ca239da
joseferunpa1 <daboto047@gmail.com>
2024-10-24 01:44:22 -0500

    Creando login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { SignInComponent } from './sign-in/sign-in.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: SignInComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' }
+];
