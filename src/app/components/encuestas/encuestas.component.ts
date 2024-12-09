4e18c57a8af44a900a468532d8c6abbef9aa03a2
HortaDeLaHoz <jhorta@unipacifico.edu.co>
2024-12-08 22:57:04 -0500

    integración del componente encuesta (singular)

--- src/app/components/encuestas/encuestas.component.ts
+++ src/app/components/encuestas/encuestas.component.ts
@@ -1,33 +1,12 @@
-import { Component,OnInit } from '@angular/core';
-import { ActionsService } from '../../services/actions.service';
-import { Router } from '@angular/router';
-import { NgIf,NgFor } from '@angular/common';
+import { Component } from '@angular/core';
+
 @Component({
   selector: 'app-encuestas',
   standalone: true,
-  imports: [NgIf,NgFor],
+  imports: [],
   templateUrl: './encuestas.component.html',
   styleUrl: './encuestas.component.scss'
 })
-export class EncuestasComponent implements OnInit {
-
-  encuestas: any[] = [];
-
-
-  constructor(private action: ActionsService, private ruta: Router) { }
-
-  ngOnInit(): void {
-    this.encuestas = this.action.getEncuestas();
-  }
-
-  iniciar(id: string): void {
-    this.ruta.navigate(['/encuesta', id]);
-  }
-  
-
-  ingresar(){
-    this.ruta.navigate(['/dashboard/list']);
-
-  }
+export class EncuestasComponent {
 
 }
